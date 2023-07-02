9538a7fe9881489a748f0669ecbc4f1684304f65
Nikita Gorin <Niki4gorin@yandex.ru>
2023-07-02 12:43:24 +0700

    Задание для самопроверки.....не прошел проверку((((

--- Program.cs
+++ Program.cs
@@ -203,11 +203,11 @@
 //             System.Console.WriteLine($"Число {number} НЕ является палиндромом");
 //           }
 //     }
-    
+
 //     {
 //     System.Console.WriteLine("Вы ввели не пятизначное число!!!"); 
 //     }
-    
+
 // }
 // System.Console.WriteLine("Введите пятизначное число:");;
 // int len = Convert.ToInt32(Console.ReadLine());
@@ -484,3 +484,74 @@
 //     Console.Write("]");
 //     Console.WriteLine();
 // }
+
+
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// int Promt(string message)
+// {
+//     System.Console.Write(message);
+//     string value = Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
+
+// int[] inputArray(int length)
+// {
+//     int[] array = new int[length];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = Promt($"Введите {i + 1} -й элемент ");
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.WriteLine($"a[{i}] = {array[i]}");
+//     }
+// }
+
+// int CountPositiveNumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+// int length = Promt("Введите количество элементов -> ");
+// int[] array;
+// array = inputArray(length);
+// PrintArray(array);
+// Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Console.WriteLine("Введите переменную b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите переменную k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите переменную b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите переменную k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// double x = -(b1 - b2) / (k1 - k2);
+// double y = k1 * x + b1;
+
+// Console.Write($"\n Точка пересечения двух прямых: [{ x},{ y}]");
